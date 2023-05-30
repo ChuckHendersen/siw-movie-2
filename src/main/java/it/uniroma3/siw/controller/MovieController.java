d12db519179c68c5db10e0c1824eeea1372eb08e
Giovanni Grieco <giovi.5057@gmail.com>
2023-05-30 10:20:29 +0200

    Continuare il lavoro sui service

--- src/main/java/it/uniroma3/siw/controller/MovieController.java
+++ src/main/java/it/uniroma3/siw/controller/MovieController.java
@@ -3,7 +3,6 @@ package it.uniroma3.siw.controller;
 import java.io.IOException;
 import java.time.Year;
 import java.util.*;
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,42 +15,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import it.uniroma3.siw.controller.validator.MovieValidator;
 import it.uniroma3.siw.model.Artist;
 import it.uniroma3.siw.model.Credentials;
 import it.uniroma3.siw.model.Movie;
-import it.uniroma3.siw.model.Picture;
 import it.uniroma3.siw.model.Review;
 import it.uniroma3.siw.repository.ArtistRepository;
-import it.uniroma3.siw.repository.MovieRepository;
-import it.uniroma3.siw.repository.PictureRepository;
-import it.uniroma3.siw.repository.ReviewRepository;
-import it.uniroma3.siw.repository.UserRepository;
 import it.uniroma3.siw.service.CredentialsService;
 import it.uniroma3.siw.service.MovieService;
 import jakarta.validation.Valid;
 
 @Controller
 public class MovieController {
-
-	@Autowired private MovieRepository movieRepository;
-	@Autowired private ArtistRepository artistRepository;
-	@Autowired private ReviewRepository reviewRepository;
-	@Autowired private MovieValidator movieValidator;
-	@Autowired private PictureRepository pictureRepository;
-	@Autowired private UserRepository userRepository;
-	@Autowired private CredentialsService credentialsService;
-
-	@Autowired private MovieService movieService;
 	
+	@Autowired private ArtistRepository artistRepository;
+	@Autowired private CredentialsService credentialsService;
+	@Autowired private MovieService movieService;
+
 	//Utility
+	/**
+	 * Metodo che ridirezione alla percorso di successo se movie!=null. Ritorna il percorso di fallimento altrimenti.
+	 * @param movie 
+	 * @param successPath Percorso di successo
+	 * @param failurePath Percorso di fallimento
+	 * @return Percorso finale
+	 */
 	public String redirection(Movie movie, String successPath, String failurePath) {
 		if(movie != null) {
 			return successPath;
 		}
 		return failurePath;
 	}
-	
+
 	@GetMapping("/")
 	public String index(Model model) {
 		return indexGeneral(model);
@@ -63,7 +57,6 @@ public class MovieController {
 	}
 
 	private String indexGeneral(Model model) {
-		//model.addAttribute("credentials", this.getCredentials());
 		return "index.html";
 	}
 
@@ -110,7 +103,7 @@ public class MovieController {
 
 	@GetMapping("/movies")
 	public String showMovies(Model model) {
-		model.addAttribute("movies",this.movieRepository.findAll());
+		model.addAttribute("movies",this.movieService.findAll());
 		return "movies.html";
 	}
 
@@ -120,7 +113,7 @@ public class MovieController {
 		model.addAttribute("credentials", credentials);
 		if(credentials!=null) {
 			//System.out.println("Utente loggato");
-			model.addAttribute("review", this.movieService.createMovie());
+			model.addAttribute("review", new Review());
 		}
 		Movie movie = this.movieService.findById(id);
 		model.addAttribute("movie", movie);
@@ -154,138 +147,73 @@ public class MovieController {
 	@GetMapping("/admin/addDirectorToMovie/{movie_id}")
 	public String addDirector(@PathVariable("movie_id") Long id, Model model) {
 		List<Artist> listaArtisti = (List<Artist>) this.artistRepository.findAll();
-		Movie movie = (Movie) this.movieRepository.findById(id).get();
-		if(listaArtisti.isEmpty()) {
-			model.addAttribute("messaggioErrore", "Non esistono artisti");
+		Movie movie = this.movieService.findById(id);
+		if(movie != null) {
+			if(listaArtisti.isEmpty()) {
+				model.addAttribute("messaggioErrore", "Non esistono artisti");
+			}else {
+				model.addAttribute("artists", listaArtisti);
+				model.addAttribute("movie", movie);
+			}
+			return "/admin/directorsToAdd.html";
 		}else {
-			model.addAttribute("artists", listaArtisti);
-			model.addAttribute("movie", movie);
+			return "movieError.html";
 		}
-		return "/admin/directorsToAdd.html";
 	}
 
 	@GetMapping("/admin/setDirectorToMovie/{director_id}/{movie_id}")
 	public String setDirectorToMovie(@PathVariable("director_id") Long director_id,@PathVariable("movie_id") Long movie_id, Model model) {
-		Artist director=null;
-		Movie movie = null;
-		try {
-			movie = (Movie)this.movieRepository.findById(movie_id).get();
-			director=(Artist)this.artistRepository.findById(director_id).get();
-			movie.setDirector(director);
-			director.getListaFilmDiretti().add(movie);
-			movieRepository.save(movie);
-			artistRepository.save(director);
-			model.addAttribute("movie", movie);
-		}catch(Exception e){
-			if(director==null) {
-				model.addAttribute("messaggioErrore", "L'artista non esiste");
-			}else if(movie==null) {
-				model.addAttribute("messaggioErrore", "L'artista non esiste");
-			}else {
-				model.addAttribute("messaggioErrore", "Errore Generico");
-			}
-		}
-		return "/admin/formUpdateMovie.html";
+		Movie movie = this.movieService.setDirectorToMovie(director_id, movie_id);
+		model.addAttribute("movie", movie);
+		return redirection(movie, "/admin/formUpdateMovie.html", "movieError.html");
 	}
 
 	@GetMapping("/admin/updateActorsOfMovie/{movie_id}")
 	public String addActors(Model model, @PathVariable("movie_id") Long movie_id) {
-		Movie movie = movieRepository.findById(movie_id).get();
-		Set<Artist> setAttoriCheNonHannoRecitato = (Set<Artist>) artistRepository.findAllByListaFilmRecitatiIsNotContaining(movie);
-		model.addAttribute("movie", movie);
-		model.addAttribute("artists", setAttoriCheNonHannoRecitato);
-		return "/admin/actorsToAdd.html";
+		Movie movie = movieService.findById(movie_id);
+		if(movie !=null) {
+			Set<Artist> setAttoriCheNonHannoRecitato = (Set<Artist>) artistRepository.findAllByListaFilmRecitatiIsNotContaining(movie);
+			model.addAttribute("movie", movie);
+			model.addAttribute("artists", setAttoriCheNonHannoRecitato);
+		}
+		return redirection(movie, "/admin/actorsToAdd.html", "movieError.html");
 	}
 
 	@GetMapping("/admin/setActorToMovie/{actor_id}/{movie_id}")
 	public String setActorToMovie(Model model, @PathVariable("actor_id") Long actor_id, @PathVariable("movie_id") Long movie_id) {
-		Movie movie = movieRepository.findById(movie_id).get();
-		Artist actor = artistRepository.findById(actor_id).get();
-		if(!movie.getActors().contains(actor)) {
-			movie.getActors().add(actor);
-			actor.getListaFilmRecitati().add(movie);
-			movieRepository.save(movie);
-			//artistRepository.save(actor); duplica le entry nel database. Fare solo 1 save?
-		}
+		Movie movie = this.movieService.setActorToMovie(actor_id, movie_id);
 		Set<Artist> setAttoriCheNonHannoRecitato = (Set<Artist>) artistRepository.findAllByListaFilmRecitatiIsNotContaining(movie);
 		model.addAttribute("movie", movie);
 		model.addAttribute("artists", setAttoriCheNonHannoRecitato);
-		return "/admin/actorsToAdd.html";
+		return redirection(movie, "/admin/actorsToAdd.html","movieError.html");
 	}
 
 	@GetMapping("/admin/deleteActorFromMovie/{actor_id}/{movie_id}")
 	public String deleteActorFromMovie(Model model, @PathVariable("actor_id") Long actor_id, @PathVariable("movie_id") Long movie_id) {
-		Movie movie = movieRepository.findById(movie_id).get();
-		Artist actor2beRemoved = artistRepository.findById(actor_id).get();
-		movie.getActors().remove(actor2beRemoved);
-		movieRepository.save(movie);
+		Movie movie = movieService.deleteActorFromMovie(actor_id, movie_id);
 		Set<Artist> setAttoriCheNonHannoRecitato = (Set<Artist>) artistRepository.findAllByListaFilmRecitatiIsNotContaining(movie);
 		model.addAttribute("movie", movie);
 		model.addAttribute("artists", setAttoriCheNonHannoRecitato);
-		return "/admin/actorsToAdd.html";
+		return redirection(movie,"/admin/actorsToAdd.html","movieError.html");
 	}
 
 	@GetMapping("/admin/moviesToDeleteIndex")
 	public String moviesToDeleteIndex(Model model) {
-		Iterable<Movie> movies = this.movieRepository.findAll();
+		Iterable<Movie> movies = this.movieService.findAll();
 		model.addAttribute("movies", movies);
 		return "/admin/movieToDeleteIndex.html";
 	}
 
 	@GetMapping("/admin/confirmMovieDeletion/{movie_id}")
 	public String confirmMovieDeletion(@PathVariable("movie_id") Long movieId, Model model) {
-		Movie movie = this.movieRepository.findById(movieId).orElse(null);
-		if(movie != null) {
-			model.addAttribute("movie", movie);
-			return "/admin/confirmMovieDeletion.html";
-		}else {
-			return "movieError.html";
-		}
+		Movie movie = this.movieService.findById(movieId);
+		return redirection(movie, "/admin/confirmMovieDeletion.html", "movieError.html");
 	}
 
 	// DA TESTARE
 	@GetMapping("/admin/deleteMovie/{movie_id}")
 	public String deleteMovie(@PathVariable("movie_id") Long movieId, Model model) {
-		Movie movieToBeDeleted = this.movieRepository.findById(movieId).orElse(null);
-		if(movieToBeDeleted != null) {
-			Set<Review> reviews = movieToBeDeleted.getReviews();
-			Set<Artist> actors = movieToBeDeleted.getActors();
-			Artist director = movieToBeDeleted.getDirector();
-			Set<Picture> pictures = movieToBeDeleted.getPictures();
-			Hibernate.initialize(reviews);
-			Hibernate.initialize(actors);
-			Hibernate.initialize(pictures);
-			movieToBeDeleted.setReviews(null);
-			movieToBeDeleted.setActors(null);
-			movieToBeDeleted.setDirector(null);
-			movieToBeDeleted.setPictures(null);
-			this.movieRepository.save(movieToBeDeleted);
-			//Eliminiamo le foto
-			for(Picture p : pictures) {
-				pictureRepository.delete(p);
-			}
-			//Eliminiamo le recensioni
-			for(Review r : reviews) {
-				r.getAuthor().getReviews().remove(r);
-				this.userRepository.save(r.getAuthor());
-				this.reviewRepository.delete(r);
-			}
-			//eliminiamo il riferimento al film recitato dell'artista
-			for(Artist actor : actors) {
-				actor.getListaFilmRecitati().remove(movieToBeDeleted);
-				artistRepository.save(actor);
-			}
-			// ripetere il processo per tutti quanti
-			// sono troppo stanco per continuare
-			//Elimiamo il riferimento del film dall'artista che lo ha diretto
-			if(director!=null) {
-				director.getListaFilmDiretti().remove(movieToBeDeleted);
-			}
-			this.movieRepository.delete(movieToBeDeleted);
-		}else {
-			return "movieError.html";
-		}
-		return "redirect:/admin/moviesToDeleteIndex";
+		return redirection(this.movieService.deleteMovie(movieId), "redirect:/admin/moviesToDeleteIndex", "movieError.html");
 	}
 
 	private Credentials getCredentials() {
--- src/main/java/it/uniroma3/siw/service/MovieService.java
+++ src/main/java/it/uniroma3/siw/service/MovieService.java
@@ -5,6 +5,7 @@ import java.time.Year;
 import java.util.List;
 import java.util.Set;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,8 +13,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 
 import it.uniroma3.siw.controller.validator.MovieValidator;
+import it.uniroma3.siw.model.Artist;
 import it.uniroma3.siw.model.Movie;
 import it.uniroma3.siw.model.Picture;
+import it.uniroma3.siw.model.Review;
 import it.uniroma3.siw.repository.ArtistRepository;
 import it.uniroma3.siw.repository.MovieRepository;
 import it.uniroma3.siw.repository.PictureRepository;
@@ -48,12 +51,12 @@ public class MovieService {
 		}
 		return null;
 	}
-	
+
 	@Transactional
 	public Movie findById(Long movieId) {
 		return this.movieRepository.findById(movieId).orElse(null);
 	}
-	
+
 	@Transactional
 	public Movie updateMovieDetails(Long movieId, Movie newMovie) {
 		Movie oldMovie = this.movieRepository.findById(movieId).orElse(null);
@@ -77,7 +80,7 @@ public class MovieService {
 		}
 		return movie;
 	}
-	
+
 	@Transactional
 	public Movie deletePhoto(Long movieId, Long pictureId) {
 		Movie movie = this.movieRepository.findById(movieId).orElse(null);
@@ -101,5 +104,90 @@ public class MovieService {
 	public Iterable<Movie> findAll() {
 		return movieRepository.findAll();
 	}
+
+	@Transactional
+	public Movie setDirectorToMovie(Long directorId, Long movieId) {
+		Movie movie = this.movieRepository.findById(movieId).orElse(null);
+		Artist director= this.artistRepository.findById(directorId).orElse(null);
+		if(movie != null) {
+			movie.setDirector(director);
+			director.getListaFilmDiretti().add(movie);
+			movieRepository.save(movie);
+			artistRepository.save(director);
+		}
+		return movie;
+	}
+
+	@Transactional
+	public Movie setActorToMovie(Long actorId, Long movieId) {
+		Movie movie = movieRepository.findById(movieId).orElse(null);
+		Artist actor = artistRepository.findById(actorId).orElse(null);
+		if(movie != null && actor != null){
+			if(!movie.getActors().contains(actor)) {
+				movie.getActors().add(actor);
+				actor.getListaFilmRecitati().add(movie);
+				movieRepository.save(movie);
+				//artistRepository.save(actor); duplica le entry nel database. Fare solo 1 save?
+			}
+			return movie;
+		}
+		return null;
+	}
+
+	@Transactional
+	public Movie deleteActorFromMovie(Long actorId, Long movieId) {
+		Movie movie = movieRepository.findById(movieId).orElse(null);
+		if(movie != null) {
+			Artist actor2beRemoved = artistRepository.findById(actorId).orElse(null);
+			if(actor2beRemoved != null) {
+				movie.getActors().remove(actor2beRemoved);
+				movieRepository.save(movie);
+				return movie;
+			}
+		}
+		return null;
+	}
 	
+	@Transactional
+	public Movie deleteMovie(Long movieId) {
+		Movie movieToBeDeleted = this.movieRepository.findById(movieId).orElse(null);
+		if(movieToBeDeleted != null) {
+			Set<Review> reviews = movieToBeDeleted.getReviews();
+			Set<Artist> actors = movieToBeDeleted.getActors();
+			Artist director = movieToBeDeleted.getDirector();
+			Set<Picture> pictures = movieToBeDeleted.getPictures();
+			Hibernate.initialize(reviews);
+			Hibernate.initialize(actors);
+			Hibernate.initialize(pictures);
+			movieToBeDeleted.setReviews(null);
+			movieToBeDeleted.setActors(null);
+			movieToBeDeleted.setDirector(null);
+			movieToBeDeleted.setPictures(null);
+			this.movieRepository.save(movieToBeDeleted);
+			//Eliminiamo le foto
+			for(Picture p : pictures) {
+				pictureRepository.delete(p);
+			}
+			//Eliminiamo le recensioni
+			for(Review r : reviews) {
+				r.getAuthor().getReviews().remove(r);
+				this.userRepository.save(r.getAuthor());
+				this.reviewRepository.delete(r);
+			}
+			//eliminiamo il riferimento al film recitato dell'artista
+			for(Artist actor : actors) {
+				actor.getListaFilmRecitati().remove(movieToBeDeleted);
+				artistRepository.save(actor);
+			}
+			// ripetere il processo per tutti quanti
+			// sono troppo stanco per continuare
+			//Elimiamo il riferimento del film dall'artista che lo ha diretto
+			if(director!=null) {
+				director.getListaFilmDiretti().remove(movieToBeDeleted);
+			}
+			this.movieRepository.delete(movieToBeDeleted);
+		}
+		return movieToBeDeleted;
+	}
 }
+
